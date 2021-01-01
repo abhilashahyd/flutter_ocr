ea97045478ad64a419edd510d5d28d0ff21af71d
prashanth kasoju <prashanth.kasoju@walkingtree.tech>
2021-01-01 12:26:56 +0530

    Complete Folder

--- flutter_text/lib/main.dart
+++ flutter_text/lib/main.dart
@@ -14,83 +14,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   File file;
   final picker = ImagePicker();
   var result;
 
   void _choose() async {
-   final  pickedFile = await picker.getImage(source: ImageSource.gallery);
-     setState(() {
-          if(pickedFile != null){
-            file = File(pickedFile.path);
-          }else{
-            print('No image selected');
-          }
-        });
-     print(file);
- }
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        file = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+    print(file);
+  }
+
 // final baseUrl = 'http://flutterocr.walkingtree.tech/';
   final baseUrl = 'http://10.0.2.2:8000';
-  var text ;
-  void getResult(res){
+  var text;
+  void getResult(res) {
     setState(() {
       text = res;
       text = jsonDecode(text);
       print(text['result']);
     });
   }
- void _upload() async{
-   print(file);
-   if (file == null) return;
-   try
-   {
-     var postUri = Uri.parse('$baseUrl/api/create');
-     http.MultipartRequest request = new http.MultipartRequest("POSt", postUri);
-     http.MultipartFile multipartFile = await http.MultipartFile.fromPath("attached_file", file.path);
-     request.files.add(multipartFile);
-     http.StreamedResponse response = await request.send();
-     print(response.statusCode);
-     response.stream.bytesToString().then((value) => getResult(value));
-   } on Error catch(e) {
-     print(e);
-   }
- }
+
+  void _upload() async {
+    print(file);
+    if (file == null) return;
+    try {
+      var postUri = Uri.parse('$baseUrl/api/create');
+      http.MultipartRequest request =
+          new http.MultipartRequest("POSt", postUri);
+      http.MultipartFile multipartFile =
+          await http.MultipartFile.fromPath("attached_file", file.path);
+      request.files.add(multipartFile);
+      http.StreamedResponse response = await request.send();
+      print(response.statusCode);
+      response.stream.bytesToString().then((value) => getResult(value));
+    } on Error catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: _choose,
-                child: Text('Choose Image'),
-              ),
-              SizedBox(width: 10.0),
-              RaisedButton(
-                onPressed: _upload,
-                child: Text('Upload Image'),
-              ),
-            ],
-          ),
-          file == null ? Text('No Image Selected') :
-          SizedBox(
-            height: 250,
-            width: 150,
-            child: Image.file(file),
-          ),
-          text == null ? Container() : Container(
-            height: 100,width: 150,
-            child: Text(text['result']),
-          ),
-        ],
+        appBar: AppBar(),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: _choose,
+                  child: Text('Choose Image'),
+                ),
+                SizedBox(width: 10.0),
+                RaisedButton(
+                  onPressed: _upload,
+                  child: Text('Upload Image'),
+                ),
+              ],
+            ),
+            file == null
+                ? Text('No Image Selected')
+                : SizedBox(
+                    height: 250,
+                    width: 150,
+                    child: Image.file(file),
+                  ),
+            text == null
+                ? Container()
+                : Container(
+                    height: 100,
+                    width: 150,
+                    child: Text(text['result']),
+                  ),
+          ],
+        ),
       ),
-    ),
     );
   }
 }
